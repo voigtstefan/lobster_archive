c18f06499175bc35181aba5b23a98fad0283e383
muffin1974 <s.voigt1988@gmx.net>
2017-02-23 13:48:58 +0100

    Changed dependency

--- R/extract_LOBSTER_files.R
+++ R/extract_LOBSTER_files.R
@@ -3,7 +3,6 @@
 #' This function extracts 7z files containing LOBSTER date (NOTE: 7z has to be included into the system PATH!) 
 #' @export
 extract_LOBSTER_files <- function(date, ticker, startdate = toDate(2007,6,27),enddate = toDate(2016,6,27),starttrade = 9.5*60*60, endtrade = 16*60*60,nlevels = 1){
-
 	zipfile <- paste(ticker,'_',strftime(startdate,'%Y-%m-%d'),'_',strftime(enddate,'%Y-%m-%d'),'_1.7z',sep='')
 	command <- paste('7z e ',zipfile,' *',strftime(date,'%Y-%m-%d'),'*.csv -r',sep='')
 	system(command)
