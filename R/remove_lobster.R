056306c1d7931c7616d51452207b26d80dbce5b8
muffin1974 <s.voigt1988@gmx.net>
2017-02-27 18:15:56 +0100

    Added remove function

---
+++ R/remove_lobster.R
@@ -0,0 +1,13 @@
+#' remove_lobster
+#'
+#' This function extracts 7z files containing LOBSTER date (NOTE: 7z has to be included into the system PATH!) 
+#' @export
+remove_lobster<-function(date,ticker,nlevels=1){
+    orderbook_file <- paste(paste(ticker, strftime(date, "%Y-%m-%d"),
+        34200000, 57600000, "orderbook", nlevels, sep = "_"),
+        "csv", sep = ".")
+    message_file <- paste(paste(ticker, strftime(date, "%Y-%m-%d"),
+        34200000, 57600000, "message", nlevels, sep = "_"), "csv",
+        sep = ".")
+	file.remove(c(message_file,orderbook_file))
+}
