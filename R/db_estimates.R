d6dc30ab09b3f8dab97fff7430a27942ee216dad
voigtstefan <s.voigt@gmx.net>
2017-04-07 08:59:46 +0200

    Changed R version dependency

--- R/db_estimates.R
+++ R/db_estimates.R
@@ -3,20 +3,24 @@
 #' lobster
 #' @export
 #' dplyr %>%
-db_estimates <- function(lobster,testing.time = unique(floor(data$Secs/15)*15)){
+db_estimates <- function(lobster,testing.time = unique(floor(data$Secs/2)*2)){
+
+  #L_m <- 5  # Parzen Kernel Bandwith
+  #L_v <- 25
+  #interval <- 5 # seconds
 
   data <- lobster%>%transmute(Secs=Secs,lret=c(NA,diff(log(Midquote))))%>%na.omit()%>%filter(lret!=0)
 
-  bndw_m <- 300
-  bndw_v <- 300
-  n_aclag <- -1
-  
+  bndw_m <- 300  # 300 seconds
+  bndw_v <- 1500 # 25 Minutes (=25*60 seconds)
+
   mu_t   <- rep(NA,length(testing.time))
-  n_lag  <- rep(NA,length(testing.time))
+  var_t  <- rep(NA,length(testing.time))
   n_mu   <- rep(NA,length(testing.time))
   n_sigma<- rep(NA,length(testing.time))
-  var_t  <- rep(NA,length(testing.time))
-  
+  n_lag  <- rep(NA,length(testing.time))
+
+
   for (i in 1:length(testing.time)){
     x <- data$Secs-testing.time[i]
     w_mu <- parzen.kernel(x/bndw_m)
@@ -25,42 +29,41 @@ db_estimates <- function(lobster,testing.time = unique(floor(data$Secs/15)*15)){
 
     w_v <- parzen.kernel(x/bndw_v)
     n_sigma[i] <- sum(w_v)
-    
+
     wdxi <- w_v[w_v > 0]*data$lret[w_v > 0]
-    v0 <- sum(wdxi^2)                          
-    
+    v0 <- sum(wdxi^2)
+
     c <- 2.6614
     max_lag <- 20
     q <- 2
     T <- n_mu[i]
-    
+
     n <- round(4*(T/100)^(4/25))
     root <- 1/(2*q+1)
-    
+
     ac <- rep(1,n)
-    
+
     for(j in 1:n){
       ac[j] <- 2*sum(wdxi[(j+1):length(wdxi)]*wdxi[1:(length(wdxi)-j)])
     }
-    
+
     s0 <- v0 + sum(ac)
     sq <- sum((1:n)^q*ac)
-             
+
     gamma <- c*(((sq/s0)^q)^root)
     n_aclag <- min(round(gamma*T^root),max_lag)
-             
+
     ac <- rep(0,n_aclag)
-             
+
     for(j in 1:n_aclag){
        ac[j] <- 2*sum(wdxi[(j+1):length(wdxi)]*wdxi[1:(length(wdxi)-j)])
     }
-    
+
     aux_hac_weight <- parzen.kernel((1:n_aclag)/n_aclag)
     var_t[i] <- v0 + sum(ac*aux_hac_weight)
-    n_lag[i] <- n_aclag         
-  }  
+    n_lag[i] <- n_aclag
+  }
   var_t <- var_t /bndw_v
   db_t <- sqrt(bndw_m)*mu_t/sqrt(var_t)
   return(list(db=db_t,mu=mu_t,var=var_t,n_mu=n_mu,n_sigma=n_sigma))
-}  
-  
+}
