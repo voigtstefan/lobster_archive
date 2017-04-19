55887b6d8f41f22e8c8c139d859842de51dced2e
voigtstefan <s.voigt1988@gmx.net>
2017-04-19 13:54:18 +0200

    Removed rounding error (testing.time was truncated)

--- R/db_estimates.R
+++ R/db_estimates.R
@@ -3,32 +3,32 @@
 #' lobster
 #' @export
 db_estimates <- function(lobster, testing.time = lobster$Secs[-c(1:5)]) {
-    
+
     # L_m <- 5 # Parzen Kernel Bandwith L_v <- 25 interval <- 5 # seconds
     data <- lobster %>% transmute(Secs = Secs, lret = c(NA, diff(log(Midquote)))) %>% na.omit() %>% filter(lret != 0)
-    
+
     bndw_m <- 300  # 300 seconds
     bndw_v <- 1500  # 25 Minutes (=25*60 seconds)
-    
-    testing.time <- testing.time[testing.time %in% data$Secs][-(1:10)]
-    
+
+    testing.time <- testing.time[testing.time > min(data$Secs) & testing.time< max(data$Secs)][-(1:10)]
+
     mu_t <- rep(NA, length(testing.time))
     var_t <- rep(NA, length(testing.time))
     n_mu <- rep(NA, length(testing.time))
     n_sigma <- rep(NA, length(testing.time))
     n_lag <- rep(NA, length(testing.time))
-    
+
     for (i in 1:length(testing.time)) {
-        dat_m <- data %>% filter(Secs < testing.time[i] & Secs > testing.time[i] - bndw_m) %>% transform(w = parzen.kernel((Secs - 
+        dat_m <- data %>% filter(Secs < testing.time[i] & Secs > testing.time[i] - bndw_m) %>% transform(w = parzen.kernel((Secs -
             testing.time[i])/bndw_m))
         n_mu[i] <- sum(dat_m$w)
         mu_t[i] <- sum(dat_m$w * dat_m$lret)/bndw_m
-        
+
         if (bndw_v != bndw_m) {
-            dat_v <- data %>% filter(Secs < testing.time[i] & Secs > testing.time[i] - bndw_v) %>% transform(w = parzen.kernel((Secs - 
+            dat_v <- data %>% filter(Secs < testing.time[i] & Secs > testing.time[i] - bndw_v) %>% transform(w = parzen.kernel((Secs -
                 testing.time[i])/bndw_v))
         }
-        if (bndw_v == bndw_m) 
+        if (bndw_v == bndw_m)
             dat_v <- dat_m
         n_sigma[i] <- sum(dat_v$w)
         wdxi <- dat_v$w * dat_v$lret
@@ -43,13 +43,13 @@ db_estimates <- function(lobster, testing.time = lobster$Secs[-c(1:5)]) {
         for (j in 1:n) {
             ac[j] <- 2 * sum(wdxi[(j + 1):length(wdxi)] * wdxi[1:(length(wdxi) - j)])
         }
-        
+
         s0 <- v0 + sum(ac)
         sq <- sum((1:n)^q * ac)
         gamma <- c * (((sq/s0)^q)^root)
         n_aclag <- min(round(gamma * T^root), max_lag, length(wdxi) - 1)
         ac <- rep(0, n_aclag)
-        
+
         for (j in 1:n_aclag) {
             ac[j] <- 2 * sum(wdxi[(j + 1):length(wdxi)] * wdxi[1:(length(wdxi) - j)])
         }
@@ -57,7 +57,7 @@ db_estimates <- function(lobster, testing.time = lobster$Secs[-c(1:5)]) {
         var_t[i] <- v0 + sum(ac * aux_hac_weight)
         n_lag[i] <- n_aclag
     }
-    
+
     var_t <- var_t/bndw_v
     db_t <- sqrt(bndw_m) * mu_t/sqrt(var_t)
     return(list(db = db_t, mu = mu_t, var = var_t, n_mu = n_mu, n_sigma = n_sigma))
