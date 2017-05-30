e2d6794cfaa09957553290cde50f9972b341b860
voigtstefan <s.voigt1988@gmx.net>
2017-05-30 17:50:38 +0200

    Added functional to avoid error with period>data available

--- R/brk_smoothing.R
+++ R/brk_smoothing.R
@@ -1,12 +1,13 @@
 #' smoothing
 #' @export
 brk_smoothing <- function(date, periods, folder='.', nobs=4){
-    all.files <- list.files(path=folder,pattern='BRK')
-    all.dates <- as.Date(strptime(all.files,'BRK_%Y-%m-%d'))
-
-    files <- all.files[which(all.dates%in%date)-0:(periods-1)]
+    all.files <- list.files(path=folder,pattern='BRK',full.names=TRUE)
+    all.dates <- as.Date(strptime(all.files,paste0(folder,'/BRK_',nobs,'_%Y-%m-%d')))
+    ind <- which(all.dates%in%date)-0:(periods-1)
+    ind <- ind[ind>0]
+    files <- all.files[ind]
     s1 <- readRDS(files[1])
-    for(s in 1:periods) s1 <- s1 + readRDS(files[s])
-    s1 <- s1/periods
+    for(s in 1:length(files)) s1 <- s1 + readRDS(files[s])
+    s1 <- s1/length(files)
     return(s1)
     }
